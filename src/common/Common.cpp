20a1a88cec9642c8f87a611110c763d8c8b2c4c5
Talha Aamir <sarcxd@gmail.com>
2021-03-01 21:56:18 +0500

    Added: New functions to common component

--- src/common/Common.cpp
+++ src/common/Common.cpp
@@ -72,4 +72,169 @@ u32_t sdbm(unsigned char *str,int len)
 		 hash = c + (hash << 6) + (hash << 16) - hash;
 	}
      return hash;
- }
+}
+
+u64_t get_current_time_us()
+{
+        static u64_t value_fix=0;
+        static u64_t largest_value=0;
+
+        u64_t raw_value=(u64_t)(ev_time()*1000*1000);
+
+        u64_t fixed_value=raw_value+value_fix;
+
+        if(fixed_value< largest_value)
+        {
+                value_fix+= largest_value- fixed_value;
+        }
+        else
+        {
+                largest_value=fixed_value;
+        }
+
+	//printf("<%lld,%lld,%lld>\n",raw_value,value_fix,raw_value + value_fix);
+        return raw_value + value_fix; //new fixed value
+}
+
+u64_t get_current_time()
+{
+	return get_current_time_us()/1000;
+}
+
+u64_t pack_u64(u32_t a,u32_t b)
+{
+	u64_t ret=a;
+	ret<<=32u;
+	ret+=b;
+	return ret;
+}
+
+u32_t get_u64_h(u64_t a)
+{
+	return a>>32u;
+}
+u32_t get_u64_l(u64_t a)
+{
+	return (a<<32u)>>32u;
+}
+
+char * my_ntoa(u32_t ip)
+{
+	in_addr a;
+	a.s_addr=ip;
+	return inet_ntoa(a);
+}
+
+/* Following 3 functions need creating dependancy functions
+u64_t get_true_random_number_64()
+{
+	u64_t ret;
+	int size=read(random_fd.get_fd(),&ret,sizeof(ret));
+	if(size!=sizeof(ret))
+	{
+		mylog(log_fatal,"get random number failed %d\n",size);
+		myexit(-1);
+	}
+	return ret;
+}
+u32_t get_true_random_number()
+{
+    u32_t ret;
+    int size=read(random_fd.get_fd(),&ret,sizeof(ret));
+    if(size!=sizeof(ret))
+    {
+        mylog(log_fatal,"get random number failed %d\n",size);
+        myexit(-1);
+    }
+return ret;
+}
+u32_t get_true_random_number_nz() //nz for non-zero
+{
+	u32_t ret=0;
+	while(ret==0)
+	{
+		ret=get_true_random_number();
+	}
+	return ret;
+}
+*/
+u64_t ntoh64(u64_t a)
+{
+	#ifdef UDP2RAW_LITTLE_ENDIAN
+		u32_t h=get_u64_h(a);
+		u32_t l=get_u64_l(a);
+		return pack_u64(ntohl(l),ntohl(h));
+		//return bswap_64( a);
+	#else
+	return a;
+	#endif
+
+}
+u64_t hton64(u64_t a)
+{
+	return ntoh64(a);
+}
+
+void write_u16(char * p,u16_t w)//@qflag what is this? and why the need to shift w, number conversion?
+{
+	*(unsigned char*)(p + 1) = (w & 0xff);
+	*(unsigned char*)(p + 0) = (w >> 8);
+}
+u16_t read_u16(char * p)
+{
+	u16_t res;
+	res = *(const unsigned char*)(p + 0);
+	res = *(const unsigned char*)(p + 1) + (res << 8);
+	return res;
+}
+
+void write_u32(char * p,u32_t l)
+{
+	*(unsigned char*)(p + 3) = (unsigned char)((l >>  0) & 0xff);
+	*(unsigned char*)(p + 2) = (unsigned char)((l >>  8) & 0xff);
+	*(unsigned char*)(p + 1) = (unsigned char)((l >> 16) & 0xff);
+	*(unsigned char*)(p + 0) = (unsigned char)((l >> 24) & 0xff);
+}
+u32_t read_u32(char * p)
+{
+	u32_t res;
+	res = *(const unsigned char*)(p + 0);
+	res = *(const unsigned char*)(p + 1) + (res << 8);
+	res = *(const unsigned char*)(p + 2) + (res << 8);
+	res = *(const unsigned char*)(p + 3) + (res << 8);
+	return res;
+}
+
+void write_u64(char * s,u64_t a)
+{
+	assert(0==1);
+}
+u64_t read_u64(char * s)
+{
+	assert(0==1);
+	return 0;
+}
+bool larger_than_u32(u32_t a,u32_t b)
+{
+	return ((i32_t(a-b)) >0);
+}
+bool larger_than_u16(uint16_t a,uint16_t b)
+{
+	return ((i16_t(a-b)) >0);
+}
+void setnonblocking(int sock) {
+	int opts;
+	opts = fcntl(sock, F_GETFL);
+
+	if (opts < 0) {
+    	mylog(log_fatal,"fcntl(sock,GETFL)\n");
+		//perror("fcntl(sock,GETFL)");
+		myexit(1);
+	}
+	opts = opts | O_NONBLOCK;
+	if (fcntl(sock, F_SETFL, opts) < 0) {
+    	mylog(log_fatal,"fcntl(sock,SETFL,opts)\n");
+		//perror("fcntl(sock,SETFL,opts)");
+		myexit(1);
+	}
+}
--- src/common/Common.h
+++ src/common/Common.h
@@ -8,8 +8,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-
+#include <string>
+#include <unistd.h>
 #include "Log.h"
+#include <assert.h>
 #include <arpa/inet.h>
 
 typedef unsigned long long u64_t;   //this works on most platform,avoid using the PRId64
@@ -28,11 +30,52 @@ typedef u64_t fd64_t;
 const int max_addr_len=100;
 extern int force_socket_buf;
 extern int g_fix_gro;
+const int show_none=0;
+const int show_command=0x1;
+const int show_log=0x2;
+const int show_all=show_command|show_log;
 class Common {
 public:
     static int executeCommand(const char*, char* &);
     u32_t djb2(unsigned char *str,int len);
     u32_t sdbm(unsigned char *str,int len);
+    u64_t get_current_time();
+    u64_t pack_u64(u32_t a,u32_t b);
+    u32_t get_u64_h(u64_t a);
+    u32_t get_u64_l(u64_t a);
+    char * my_ntoa(u32_t ip);
+    u64_t get_true_random_number_64();
+    u32_t get_true_random_number();
+    u32_t get_true_random_number_nz();
+    u64_t ntoh64(u64_t a);
+    u64_t hton64(u64_t a);
+    void write_u16(char *,u16_t a);// network order
+    u16_t read_u16(char *);
+    void write_u32(char *,u32_t a);// network order
+    u32_t read_u32(char *);
+    void write_u64(char *,u64_t a);
+    u64_t read_u64(char *);
+    bool larger_than_u16(uint16_t a,uint16_t b);
+    bool larger_than_u32(u32_t a,u32_t b);
+    void setnonblocking(int sock);
+    int set_buf_size(int fd,int socket_buf_size);
+    void myexit(int a);
+    unsigned short csum(const unsigned short *ptr,int nbytes);
+    unsigned short csum_with_header(char* header,int hlen,const unsigned short *ptr,int nbytes);
+    int numbers_to_char(my_id_t id1,my_id_t id2,my_id_t id3,char * &data,int &len);
+    int char_to_numbers(const char * data,int len,my_id_t &id1,my_id_t &id2,my_id_t &id3);
+    int run_command(string command,char * &output,int flag=show_all);
+    //int run_command_no_log(string command,char * &output);
+    int read_file(const char * file,string &output);
+    vector<string> string_to_vec(const char * s,const char * sp);
+    vector< vector <string> > string_to_vec2(const char * s);
+    string trim(const string& str, char c);
+    string trim_conf_line(const string& str);
+    vector<string> parse_conf_line(const string& s);
+    int hex_to_u32_with_endian(const string & a,u32_t &output);
+    int hex_to_u32(const string & a,u32_t &output);
+    int create_fifo(char * file);
+    void print_binary_chars(const char * a,int len);
 };
 
 #endif //RAWSOCKET_COMMON_H
